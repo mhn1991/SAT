2ca4547d88beb1ed1a0fe5726976e75ab762ce45
mhn1991 <mhn_1991@yahoo.com>
2016-07-07 00:15:38 +0100

    Update Simdpll.thy
    fixing resolve function

--- Simdpll.thy
+++ Simdpll.thy
@@ -6,38 +6,25 @@ type_synonym    Literal =  int
 type_synonym  Clause =  "Literal list" 
 type_synonym Formula = "Clause list"
 
-fun negateLiteral :: "Literal  \<Rightarrow>  Literal "
+fun negateLiteral :: "Literal  ⇒  Literal "
 where
 "negateLiteral a = (-1*a)"
 
-fun findUnit :: "Formula \<Rightarrow> Literal"
+fun findUnit :: "Formula ⇒ Literal"
 where
 "findUnit (c#cs) = (if length c = 1 then  (hd c) else findUnit cs)"
 |
 "findUnit []     = undefined"
 
-fun notElem :: " Literal \<Rightarrow> Clause \<Rightarrow> bool" 
+fun notElem :: " Literal ⇒ Clause ⇒ bool" 
 where
 "notElem _ [] = True" 
 |
 "notElem p (x#xs) = (if p=x then False else notElem p xs)"
 
-fun elem :: "Literal \<Rightarrow> Clause \<Rightarrow> bool"
-where 
-" elem _ [] = False"
-|
-"elem p (x#xs) = (if p=x then True else elem p xs)"
-
-
-fun remove':: "Literal  \<Rightarrow> Formula \<Rightarrow> Formula"
+fun resolve ::"Formula ⇒ Literal ⇒ Formula"
 where
-"remove' _ [] = []"
-|
-"remove' l (x#xs) = (if (elem l x) then  (removeAll l x)# xs else (remove' l xs)) "
-
-fun resolve ::"Formula \<Rightarrow> Literal \<Rightarrow> Formula"
-where
-"resolve f literal =(let q = filter (notElem literal) f in remove' literal q)"
+"resolve f literal =(let q = filter (notElem literal) f in map (removeAll (negateLiteral literal)) q)"
 
 
 end
