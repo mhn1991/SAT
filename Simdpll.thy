6793fc9598c2ff80e1953eaee1e7b05dcae07db7
mhn1991 <mhn_1991@yahoo.com>
2016-07-11 10:27:39 +0100

    Update Simdpll.thy
    Implementation has been finished.

--- Simdpll.thy
+++ Simdpll.thy
@@ -1,30 +1,51 @@
 theory Simdpll 
-imports Main 
+imports Main
+
 begin
-datatype 'a Option  = None | Some 'a
+datatype 'a Option      = None | Some 'a
 type_synonym    Literal =  int  
-type_synonym  Clause =  "Literal list" 
-type_synonym Formula = "Clause list"
+type_synonym  Clause    =  "Literal list" 
+type_synonym Formula    = "Clause list"
 
-fun negateLiteral :: "Literal  ⇒  Literal "
+fun negateLiteral :: "(Literal) ⇒  Literal "
 where
-"negateLiteral a = (-1*a)"
+"negateLiteral  a =   (-1*a)"
 
-fun findUnit :: "Formula ⇒ Literal"
+fun findUnit :: "Formula ⇒ (Literal) Option "
 where
-"findUnit (c#cs) = (if length c = 1 then  (hd c) else findUnit cs)"
+"findUnit [] = None"
 |
-"findUnit []     = undefined"
+"findUnit (c#cs) = (if length c = 1 then Some (hd c) else findUnit cs)"
 
-fun notElem :: " Literal ⇒ Clause ⇒ bool" 
+fun notElem :: "(Literal) ⇒ Clause ⇒ bool" 
 where
 "notElem _ [] = True" 
 |
 "notElem p (x#xs) = (if p=x then False else notElem p xs)"
 
-fun resolve ::"Formula ⇒ Literal ⇒ Formula"
+fun elem :: "Clause ⇒ Formula ⇒bool"
 where
-"resolve f literal =(let q = filter (notElem literal) f in map (removeAll (negateLiteral literal)) q)"
+"elem _ [] = False"
+|
+"elem p (x#xs) = (if p=x then True else elem p xs)"
 
+(*should work on termination of this function *)
+fun resolve ::"Formula ⇒ (Literal) ⇒ (Formula)"
+where
+"resolve f literal =(let  q = filter (notElem literal) f in map (removeAll (negateLiteral literal)) q)"
+
+
+(*second section of case has problem*)
+function unitPropagate:: "Formula ⇒ Formula "
+where
+"unitPropagate f = (case (findUnit f) of None ⇒  f  | Some a ⇒ unitPropagate (resolve f a))"
+apply auto
+done
+
+function dpll:: "Formula ⇒ bool"
+where
+"dpll f = (if f = [] then True else if (elem [] f) then False else  (let ff = unitPropagate f in(let nextLiteral = hd( hd f) in dpll (resolve f nextLiteral) | dpll (resolve f (negateLiteral nextLiteral)))))"  
+apply auto 
+done
 
 end
