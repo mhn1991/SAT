d25323c140f4f49991f1331d8834833b78786985
mohammadHossein Nourian <mhn_1991@yahoo.com>
2016-07-24 02:28:14 +0100

    prove one of lemma from two to show termination of the dpll function there is counter example for the second one. I have to fix it.

--- Isabelle/Simdpll.thy
+++ Isabelle/Simdpll.thy
@@ -8,7 +8,7 @@ type_synonym    Literal =  int
 type_synonym  Clause    =  "Literal list" 
 type_synonym Formula    = "Clause list"
 
-fun negateLiteral :: "(Literal) ⇒  Literal "
+fun negateLiteral :: "(Literal) \<Rightarrow>  Literal "
 where
 "negateLiteral  a =   (-1*a)"
 
@@ -16,13 +16,13 @@ value "negateLiteral 1"
 value "negateLiteral 0"
 value "negateLiteral (-1::int)"
 
-fun deOption::"(Formula) Option ⇒ Formula"
+fun deOption::"(Formula) Option \<Rightarrow> Formula"
 where
 "deOption None = undefined"
 |
 "deOption (Some f) =  f" 
 
-fun findUnit :: "Formula ⇒ (Literal) Option "
+fun findUnit :: "Formula \<Rightarrow> (Literal) Option "
 where
 "findUnit [] = None"
 |
@@ -33,7 +33,7 @@ value "findUnit [[],[1,2],[2]]"
 value "findUnit [[1,2]]"
 
 
-fun notElem :: "(Literal) ⇒ Clause ⇒ bool" 
+fun notElem :: "(Literal) \<Rightarrow> Clause \<Rightarrow> bool" 
 where
 "notElem _ [] = True" 
 |
@@ -43,16 +43,16 @@ value "notElem 1 [1,2,3]"
 value "notElem 4 [1,2,3]"
 value "notElem 1 [] "
 
-fun elem :: "Clause ⇒ Formula ⇒bool"
+fun elem :: "Clause \<Rightarrow> Formula \<Rightarrow>bool"
 where
 "elem _ [] = False"
 |
 "elem p (x#xs) = (if p=x then True else elem p xs)"
 
 
-
+value "elem [] [[]]"
 (*should work on termination of this function *)
-fun resolve ::"Formula ⇒ Literal ⇒ Formula"
+fun resolve ::"Formula \<Rightarrow> Literal \<Rightarrow> Formula"
 where
 "resolve f literal =  (let  q = filter (notElem literal) f in map (removeAll (negateLiteral literal)) q)"
 
@@ -63,7 +63,9 @@ lemma 1[simp]: "resolve [] a = []"
 apply simp
 done
 
-lemma ter: "findUnit f = Some x ⟹ length (filter (notElem x) f) < length f"
+
+
+lemma yes: "findUnit f = Some x \<Longrightarrow> length (filter (notElem x) f) < length f"
 proof (induction f)
   case Nil
   then show ?case by simp
@@ -72,7 +74,35 @@ next
   fix f a
   assume
     prems: "findUnit (a # f) = Some x" and
-    IH: "findUnit f = Some x ⟹ length (filter (notElem x) f) < length f"
+    IH: "findUnit f = Some x \<Longrightarrow> length (filter (notElem x) f) < length f"
+  then show "length (filter (notElem x) (a # f)) < length (a # f)"
+  proof (cases "notElem x a")
+    case
+      H1: True
+    then show ?thesis
+    proof (cases "findUnit f = Some x")
+      case True
+      with IH show ?thesis by simp
+    next
+      case False
+      then show ?thesis by (metis H1 One_nat_def Option.inject findUnit.simps(2) list.sel(1) list.size(3) n_not_Suc_n notElem.elims(2) prems)
+qed 
+next case False
+then show ?thesis by (simp add: filter.simps(2) le_imp_less_Suc length_Cons length_filter_le)
+qed
+qed
+
+
+lemma ter: "findUnit f = Some x \<Longrightarrow> length (filter (notElem x) f) < length f"
+proof (induction f)
+  case Nil
+  then show ?case by simp
+next
+  case Cons
+  fix f a
+  assume
+    prems: "findUnit (a # f) = Some x" and
+    IH: "findUnit f = Some x \<Longrightarrow> length (filter (notElem x) f) < length f"
   then show "length (filter (notElem x) (a # f)) < length (a # f)"
   proof (cases "notElem x a")
     case
@@ -85,7 +115,7 @@ next
       case False
       then have "Some (hd a) = Some x" "length a = 1"
         using findUnit.simps prems by presburger+
-      then have "¬ notElem x a" by (induction a) simp_all
+      then have "\<not> notElem x a" by (induction a) simp_all
       with H1 show ?thesis by simp
     qed
   next
@@ -96,46 +126,79 @@ qed
 
 
 (*second section of case has problem*)
-function  unitPropagate:: "Formula ⇒ Formula "
+function  unitPropagate:: "Formula \<Rightarrow> Formula "
 where 
-"unitPropagate f = (case (findUnit f) of None ⇒  f  | Some a ⇒ unitPropagate (resolve f a))"
+"unitPropagate f = (case (findUnit f) of None \<Rightarrow>  f  | Some a \<Rightarrow> unitPropagate (resolve f a))"
 by pat_completeness auto
 
 termination 
-apply (relation "measure (λx. size x)")
+apply (relation "measure (\<lambda>x. size x)")
 apply (auto)
-apply (simp add: ter)
+apply (simp add: yes)
 done
-
-
-
-
-
-
 
 value "unitPropagate [[1,2],[1],[1,2]]"
 value "unitPropagate [[1,2,3]]"
 
-function dpll:: "Formula ⇒ bool"
+lemma ter2: "f \<noteq> [] \<Longrightarrow> \<not> elem [] f \<Longrightarrow> length (filter (notElem (hd (hd f))) f) < length f"
+proof (induction f)
+case Nil
+then show ?case by simp
+next
+  case Cons
+  fix f a 
+ assume
+    prems: "(a#f) \<noteq> []" and
+    premss: " \<not> elem [] (a#f)" and
+    IM: "f \<noteq> [] \<Longrightarrow> \<not> elem [] f \<Longrightarrow> length (filter (notElem (hd (hd f))) f) < length f"
+    then show "length (filter (notElem (hd (hd (a#f)))) (a#f)) < length (a#f)"
+    proof (cases "notElem (hd (hd (a#f))) a") 
+    case 
+    H1: True
+    then show ?thesis
+    proof (cases "(a#f) \<noteq> []")
+    case True
+    then show ?thesis  
+    proof (cases"\<not> elem [] (a#f)" )
+    case True
+    then show ?thesis by (metis H1 elem.simps(2) list.sel(1) notElem.elims(2))
+      next 
+    case False
+    then show ?thesis using premss by blast
+qed
+  next 
+    case False
+        then show ?thesis by simp
+        qed
+next 
+case False 
+then show ?thesis by (simp add: filter.simps(2) le_imp_less_Suc length_Cons length_filter_le)
+qed
+qed
+
+lemma ter3: "f \<noteq> [] \<Longrightarrow> \<not> elem [] f \<Longrightarrow> length (filter (notElem (- hd (hd f))) f) < length f"
+oops
+
+
+fun slm ::"Formula \<Rightarrow> Formula"
 where
-"dpll f = (if f = [] then True else if (elem [] f) then False else  (let ff = unitPropagate f in(let nextLiteral = hd( hd f) in dpll (resolve f nextLiteral) | dpll (resolve f (negateLiteral nextLiteral)))))"
-by pat_completeness auto 
-termination sorry
+"slm f = filter (notElem (-hd (hd f))) f"
 
-value "dpll [[1],[2],[(-1),(-2)]]"
-value "dpll [[]]"
+value "slm [[-1]]"
 
-function fib :: "nat ⇒ nat" where
-"fib 0 = 0" |
-"fib (Suc 0) = 1" |
-"fib (Suc(Suc x)) = fib x + fib (Suc x)"
-
+function dpll:: "Formula \<Rightarrow> bool"
+where
+"dpll f = (if f = [] then True else if (elem [] f) then False else (let ff = unitPropagate f in(let nextLiteral = hd( hd f) in dpll (resolve f nextLiteral) \<or> dpll (resolve f (negateLiteral nextLiteral)))))"
 by pat_completeness auto 
 
-termination 
-apply (relation "measure (λx. x)") 
+termination
+apply (relation "measure (\<lambda> x. size x)")
 apply auto
+apply (simp add : ter2)
+apply try
 done
 
 
+value "dpll [[1],[2],[(-1),(-2)]]"
+value "dpll [[-1]]"
 end
